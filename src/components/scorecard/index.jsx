dfdcc7f27918ab0a8901039429c2758f6123b6a4
chetan-90 <133109083+chetan-90@users.noreply.github.com>
2023-08-24 14:39:29 +0530

    duo

--- src/components/scorecard/index.jsx
+++ src/components/scorecard/index.jsx
@@ -1,43 +1,68 @@
-import React from 'react'
-import { useEffect,useRef,useState } from 'react'
-import Control from '../control'
-import { useSelector } from 'react-redux'
-import {  Wrapper } from './style'
+import React, { useEffect, useRef, useState } from 'react';
+import Control from '../control';
+import { Wrapper } from './style';
+import { useSelector } from 'react-redux';
 
 const Scorecard = () => {
   const team1 = useSelector(state => state.team1);
   const team2 = useSelector(state => state.team2);
-    const [score, setScore] = useState(0)
-  const [balls, setBalls] = useState(0)
-  const [wickets, setWickets] = useState(0)
-  const [timeline, setTimeline] = useState([])
+  const [score, setScore] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const [wickets, setWickets] = useState(0);
+  const [timeline, setTimeline] = useState([]);
+  const [inning, setInning] = useState(1);
+  const [target, setTarget] = useState(null);
 
 
   useEffect(() => {
-    if(wickets === 10){
-      alert(`Inning Done !`)
+    if (wickets === 10) {
+      alert(`Inning Done !`);
+      if (inning === 1) {
+        setTarget(score + 1);
+        setInning(2);
+        setScore(0);
+        setBalls(0);
+        setWickets(0);
+        setTimeline([]);
+      }    
     }
-  },[wickets])
+    if (inning === 2 && score >= target) {
+      alert('Second team wins!');
+    } else if (inning === 2 && score === target-1  && wickets === 10) {
+      alert('It\'s a tie!');
+    } else if(inning ===2 && wickets === 10) {
+      alert( 'first wins!');
+    
+  }
+  }, [wickets, inning, score, target]);
 
-  const timelineRef = useRef()
+  const timelineRef = useRef();
+
   return (
     <div>
       <Wrapper>
       <div id = "teams">
-          <p>Team 1: {team1}</p>
-          <p>Team 2: {team2}</p>
+          <p>Batting Team : {team1}</p>
+          <p>Bowling Team : {team2}</p>
+        
         </div>
-        < div className='inner'>
+        <div className='inner'>
+          <h2>Scorecard </h2>
           <p>
-          Score : {score}/{wickets}
+            Score : {score}/{wickets}
           </p>
           <p>
-            Overs : {`${Math.floor(balls/6)}.${balls%6}`}
+            Overs : {`${Math.floor(balls / 6)}.${balls % 6}`}
+          </p>
+          <p>
+            {inning === 2 && target !== null &&(
+              <div>Target : {target}</div>
+            )}
           </p>
           <div className='timeline' ref={timelineRef}>
-            {
-              timeline.map(item => <div className='timeline-item'>{item}</div>)
-            }
+            {timeline.map(item => (
+              <div className='timeline-item'>{item}</div>
+            ))}
           </div>
         </div>
       </Wrapper>
@@ -46,9 +71,14 @@ const Scorecard = () => {
         setBalls={setBalls}
         setWickets={setWickets}
         setTimeline={setTimeline}
+        score={score}
+        wickets={wickets}
+        setTarget={setTarget}
+        target={target}
+        inning={inning}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Scorecard
+export default Scorecard;
